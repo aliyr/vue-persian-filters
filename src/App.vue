b247c2aa48b1fa9b06b025e8d8dedd07660740c7
amin.atashzar.1990@gmail.com <amin.atashzar.1990@gmail.com>
2021-04-19 11:06:02 +0430

    remove extra codes from app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-     {{num | toPersianNumber}}
+    <!-- {{num | toPersianNumber}}
     <br>
     <span> {{date | toPersianDate(' Do MMMM YYYY')}} تاریخ ماست </span>
     <br>
@@ -14,28 +14,28 @@
     <br>
     <span>{{num2 | toPersianDigits}}</span>
     <br>
-    <button @click="increase">increase </button>
+    <button @click="increase">increase </button> -->
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app',
+  name: "app",
   data: function () {
     return {
       num: -1243234.588,
       date: new Date(),
-      cur: 'تومان',
-      words: 'در اداره ی شهرداری جنوب تهران',
-      num2: '1534221'
-    }
+      cur: "تومان",
+      words: "در اداره ی شهرداری جنوب تهران",
+      num2: "1534221",
+    };
   },
   methods: {
-    increase () {
-      this.num++
-    }
-  }
-}
+    increase() {
+      this.num++;
+    },
+  },
+};
 </script>
 
 <style lang="scss"></style>
